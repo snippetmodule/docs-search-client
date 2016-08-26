5eb38fc9d0ed1e25fb4f2e1dc58fc13da0148187
wangfaxi <wangfaxi@cmcm.com>
2016-08-26 21:11:26 +0800

    server:增加typings

--- server/typings/index.d.ts
+++ server/typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/async/index.d.ts" />
 /// <reference path="globals/bunyan/index.d.ts" />
 /// <reference path="globals/fs-extra/index.d.ts" />
 /// <reference path="globals/isomorphic-fetch/index.d.ts" />
