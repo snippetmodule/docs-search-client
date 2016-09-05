a613a8ca73e5b9d377e593caade24d5a351bbaca
wangfaxi <wangfaxi@cmcm.com>
2016-09-05 15:40:40 +0800

    client:修改搜索结果样式

--- client/src/app/containers/App/left.tsx
+++ client/src/app/containers/App/left.tsx
@@ -5,28 +5,53 @@ import {ISearchResultItem} from '../../core/model';
 import {ISearchState} from '../../redux/reducers/searchdocs';
 import ReactList from '../../utils/react-lists';
 import {DefaultList} from './list';
+import {history} from '../../routes';
 const { connect } = require('react-redux');
 
 interface ISearchProps {
     searchState?: ISearchState;
 }
+function getItemCss(deep: number, isSelected: boolean) {
+    return {
+        paddingTop: '.25rem',
+        paddingBottom: '.25rem',
+        paddingLeft: 0.75 * (deep + 1) + 'rem',
+        paddingRight: '0.75rem',
+        background: isSelected ? '#398df0' : '#f9f9f9',
+        boxShadow: 'inset -1px 0 #e3e3e3',
+        width: '100%',
+        display: 'block',
+        position: 'relative',
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        wordSrap: 'normal',
+        overflowSrap: 'normal',
+        textOverflow: 'ellipsis',
+        textDecoration: 'none',
+    };
+}
 
 @connect(state => ({ searchState: state.searchDocsReducer }))
 class Left extends React.Component<ISearchProps, void> {
-    // public shouldComponentUpdate(nextProps: ISearchProps, nextState: void, nextContext: any): boolean {
-    //     return this.props.searchState.input !== nextProps.searchState.input;
-    // }
+    private selectedIndex = 0;
+
+    private onClickItem(index: number, searchResultItem: ISearchResultItem) {
+        this.selectedIndex = index;
+        this.forceUpdate();
+        history.push({
+            pathname: '/docs/' + searchResultItem.doc.slug + '/' + (searchResultItem.path ? searchResultItem.path : searchResultItem.slug + '/'),
+        });
+    }
     private renderItem(index, key) {
         let searchResultItem: ISearchResultItem = this.props.searchState.message[index];
-        if (searchResultItem.path) {
-            return (<li key={key}> <Link  to={{ pathname: '/docs/' + searchResultItem.doc.slug + '/' + searchResultItem.path }} > { searchResultItem.name}</Link ></li>);
-        } else {
-            return (<li key={key}> <Link  to={{ pathname: '/docs/' + searchResultItem.doc.slug + '/' + searchResultItem.slug + '/' }} > { searchResultItem.name}</Link ></li>);
-        }
-        // return <div key={key}>{'index ' + index + 'key' + key}</div>;
+        return (
+            <div key={key} to="" style={getItemCss(0, index === this.selectedIndex) }
+                onClick={event => { event.preventDefault(); this.onClickItem(index, searchResultItem); } }>
+                {searchResultItem.name}
+            </div>
+        );
     }
     public render() {
-        console.log('Left .....' + this.props.searchState.input + ' time:' + new Date().getTime());
         if (!this.props.searchState.input) {
             return (<DefaultList />);
         }
@@ -42,13 +67,13 @@ class Left extends React.Component<ISearchProps, void> {
             return (<div> 未找到搜索结果 </div>);
         }
         return (
-            <ul style={{ overflowY: 'scroll', height: '100%', overflowX: 'hidden' }}>
+            <div style={{ paddingTop: '0.5rem', paddingBottom: '0.5rem', height: '100%', boxShadow: 'inset -1px 0 #e3e3e3' }}>>
                 <ReactList
                     itemRenderer={this.renderItem.bind(this) }
                     length={50}
                     type ="uniform"
                     />
-            </ul>);
+            </div>);
     }
 }
 
